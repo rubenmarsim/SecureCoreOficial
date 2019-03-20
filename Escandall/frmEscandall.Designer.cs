ae41d847f726aa881f1dc4eb9e47e68f38909848
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-03-20 20:50:46 +0100

    escandall final y middle prods ok

---
+++ Escandall/frmEscandall.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Escandall
+{
+    partial class frmEscandall
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeViewEscandall = new System.Windows.Forms.TreeView();
+            this.btnPadre = new System.Windows.Forms.Button();
+            this.btnHijo = new System.Windows.Forms.Button();
+            this.btnBorrarCampo = new System.Windows.Forms.Button();
+            this.btnBorrarArbol = new System.Windows.Forms.Button();
+            this.txtBoxAddCampo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeViewEscandall
+            // 
+            this.treeViewEscandall.Location = new System.Drawing.Point(12, 12);
+            this.treeViewEscandall.Name = "treeViewEscandall";
+            this.treeViewEscandall.Size = new System.Drawing.Size(375, 426);
+            this.treeViewEscandall.TabIndex = 0;
+            // 
+            // btnPadre
+            // 
+            this.btnPadre.Location = new System.Drawing.Point(591, 246);
+            this.btnPadre.Name = "btnPadre";
+            this.btnPadre.Size = new System.Drawing.Size(197, 23);
+            this.btnPadre.TabIndex = 1;
+            this.btnPadre.Text = "Crear como padre";
+            this.btnPadre.UseVisualStyleBackColor = true;
+            this.btnPadre.Click += new System.EventHandler(this.btnPadre_Click);
+            // 
+            // btnHijo
+            // 
+            this.btnHijo.Location = new System.Drawing.Point(591, 297);
+            this.btnHijo.Name = "btnHijo";
+            this.btnHijo.Size = new System.Drawing.Size(197, 23);
+            this.btnHijo.TabIndex = 2;
+            this.btnHijo.Text = "Crear como hijo";
+            this.btnHijo.UseVisualStyleBackColor = true;
+            this.btnHijo.Click += new System.EventHandler(this.btnHijo_Click);
+            // 
+            // btnBorrarCampo
+            // 
+            this.btnBorrarCampo.Location = new System.Drawing.Point(591, 356);
+            this.btnBorrarCampo.Name = "btnBorrarCampo";
+            this.btnBorrarCampo.Size = new System.Drawing.Size(197, 23);
+            this.btnBorrarCampo.TabIndex = 3;
+            this.btnBorrarCampo.Text = "Borrar campo seleccionado";
+            this.btnBorrarCampo.UseVisualStyleBackColor = true;
+            this.btnBorrarCampo.Click += new System.EventHandler(this.btnBorrarCampo_Click);
+            // 
+            // btnBorrarArbol
+            // 
+            this.btnBorrarArbol.Location = new System.Drawing.Point(591, 412);
+            this.btnBorrarArbol.Name = "btnBorrarArbol";
+            this.btnBorrarArbol.Size = new System.Drawing.Size(197, 23);
+            this.btnBorrarArbol.TabIndex = 4;
+            this.btnBorrarArbol.Text = "Borrar todo el arbol";
+            this.btnBorrarArbol.UseVisualStyleBackColor = true;
+            this.btnBorrarArbol.Click += new System.EventHandler(this.btnBorrarArbol_Click);
+            // 
+            // txtBoxAddCampo
+            // 
+            this.txtBoxAddCampo.Location = new System.Drawing.Point(393, 189);
+            this.txtBoxAddCampo.Name = "txtBoxAddCampo";
+            this.txtBoxAddCampo.Size = new System.Drawing.Size(395, 20);
+            this.txtBoxAddCampo.TabIndex = 5;
+            this.txtBoxAddCampo.TextChanged += new System.EventHandler(this.txtBoxAddCampo_TextChanged);
+            // 
+            // frmEscandall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBoxAddCampo);
+            this.Controls.Add(this.btnBorrarArbol);
+            this.Controls.Add(this.btnBorrarCampo);
+            this.Controls.Add(this.btnHijo);
+            this.Controls.Add(this.btnPadre);
+            this.Controls.Add(this.treeViewEscandall);
+            this.Name = "frmEscandall";
+            this.Text = "Escandall";
+            this.Load += new System.EventHandler(this.frmEscandall_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewEscandall;
+        private System.Windows.Forms.Button btnPadre;
+        private System.Windows.Forms.Button btnHijo;
+        private System.Windows.Forms.Button btnBorrarCampo;
+        private System.Windows.Forms.Button btnBorrarArbol;
+        private System.Windows.Forms.TextBox txtBoxAddCampo;
+    }
+}
+
---
+++ Escandall/frmEscandall.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escandall
+{
+    public partial class frmEscandall : Form
+    {
+        #region Variables Globales
+        GestionDB.XWingsFactoryEntities db;
+        List<string> _lstFinalProducts;
+        List<string> _lstMiddleProducts;
+        List<GestionDB.References> _lstRawMaterials;
+        /// <summary>
+        /// Definicion de cada id del campo idRederenceType
+        /// </summary>
+        enum eReferenceTypes
+        {
+            FinalProducts = 1,
+            MiddleProducts = 2,
+            RawMaterials = 3,
+        }
+        enum eIdReference
+        {
+            Ala = 19,
+            Motor = 22,
+            Cabina = 23,
+            Morro = 24
+        }
+        #endregion
+
+        #region Constructores
+        public frmEscandall()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        private void frmEscandall_Load(object sender, EventArgs e)
+        {
+            db = new GestionDB.XWingsFactoryEntities();
+            GetData();
+            LoadData();
+            CanExecute();
+        }
+
+        private void btnPadre_Click(object sender, EventArgs e)
+        {
+                treeViewEscandall.Nodes.Add(txtBoxAddCampo.Text);
+                txtBoxAddCampo.Clear();            
+        }
+
+        private void btnHijo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                treeViewEscandall.SelectedNode.Nodes.Add(txtBoxAddCampo.Text);
+                txtBoxAddCampo.Clear();
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Selecciona un padre para poder crear el hijo anidado a este.");
+            }
+        }
+
+        private void btnBorrarCampo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                treeViewEscandall.Nodes.Remove(treeViewEscandall.SelectedNode);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Selecciona el campo que quieres borrar");
+            }
+            
+        }
+
+        private void btnBorrarArbol_Click(object sender, EventArgs e)
+        {
+            treeViewEscandall.Nodes.Clear();
+        }
+        private void txtBoxAddCampo_TextChanged(object sender, EventArgs e)
+        {
+            CanExecute();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Comprueba si el textbox esta vacio o no y activa los 
+        /// botones de crear en funcion de eso
+        /// </summary>
+        private void CanExecute()
+        {
+            if (string.IsNullOrEmpty(txtBoxAddCampo.Text))
+            {
+                btnPadre.Enabled = false;
+                btnHijo.Enabled = false;
+            }
+            else
+            {
+                btnPadre.Enabled = true;
+                btnHijo.Enabled = true;
+            }
+        }
+
+        private void GetData()
+        {
+            _lstFinalProducts = db.References.Where(x => x.idReferenceType == (short)eReferenceTypes.FinalProducts).Select(x => x.descReference).ToList();
+            _lstMiddleProducts = db.References.Where(x => x.idReferenceType == (short)eReferenceTypes.MiddleProducts).Select(x => x.descReference).ToList();
+            _lstRawMaterials = db.References.Where(x => x.idReferenceType == (short)eReferenceTypes.RawMaterials).Select(x => x).ToList();
+        }
+        private void LoadData()
+        {
+            //--- Productos Principales ---------------------------------------------
+            treeViewEscandall.Nodes.Add(_lstFinalProducts[0]);
+            //--- Productos intermedios y materia prima -----------------------------
+            foreach(var middleProd in _lstMiddleProducts)
+            {
+                treeViewEscandall.Nodes[0].Nodes.Add(middleProd);
+
+                foreach(var RawProd in _lstRawMaterials)
+                {
+                    if(RawProd.idReference == (short)eIdReference.Ala)
+                        treeViewEscandall.Nodes[0].Nodes[0].Nodes.Add(RawProd.descReference);
+                    else if(RawProd.idReference == (short)eIdReference.Motor)
+                        treeViewEscandall.Nodes[0].Nodes[1].Nodes.Add(RawProd.descReference);
+                    else if (RawProd.idReference == (short)eIdReference.Cabina)
+                        treeViewEscandall.Nodes[0].Nodes[2].Nodes.Add(RawProd.descReference);
+                    else if (RawProd.idReference == (short)eIdReference.Morro)
+                        treeViewEscandall.Nodes[0].Nodes[3].Nodes.Add(RawProd.descReference);
+                }                
+            }
+        }
+        #endregion
+
+
+    }
+}
