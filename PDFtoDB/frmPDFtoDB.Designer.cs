c85d4ce16548c40c3b2d27abb6d76ac80bfeedf1
Toonii <toni.arandasds@gmail.com>
2019-03-28 18:13:08 +0100

    PDFtoDB
    Hecho con errores

---
+++ PDFtoDB/frmPDFtoDB.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace PDFtoDB
+{
+    partial class frm_PDFtoDB
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgv_PDFtoDB = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Nombre = new System.Windows.Forms.TextBox();
+            this.textBox_Archivo = new System.Windows.Forms.TextBox();
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PDFtoDB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Open);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Save);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Browse);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox_Archivo);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox_Nombre);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgv_PDFtoDB);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 169;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgv_PDFtoDB
+            // 
+            this.dgv_PDFtoDB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_PDFtoDB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_PDFtoDB.Location = new System.Drawing.Point(0, 0);
+            this.dgv_PDFtoDB.Name = "dgv_PDFtoDB";
+            this.dgv_PDFtoDB.Size = new System.Drawing.Size(800, 277);
+            this.dgv_PDFtoDB.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Identificador:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Archivo:";
+            // 
+            // textBox_Nombre
+            // 
+            this.textBox_Nombre.Location = new System.Drawing.Point(92, 27);
+            this.textBox_Nombre.Name = "textBox_Nombre";
+            this.textBox_Nombre.Size = new System.Drawing.Size(87, 20);
+            this.textBox_Nombre.TabIndex = 3;
+            // 
+            // textBox_Archivo
+            // 
+            this.textBox_Archivo.Location = new System.Drawing.Point(92, 69);
+            this.textBox_Archivo.Name = "textBox_Archivo";
+            this.textBox_Archivo.Size = new System.Drawing.Size(543, 20);
+            this.textBox_Archivo.TabIndex = 4;
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.Location = new System.Drawing.Point(641, 67);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(32, 23);
+            this.btn_Browse.TabIndex = 5;
+            this.btn_Browse.Text = "...";
+            this.btn_Browse.UseVisualStyleBackColor = true;
+            this.btn_Browse.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(598, 112);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 6;
+            this.btn_Save.Text = "Guardar";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(679, 112);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(75, 23);
+            this.btn_Open.TabIndex = 7;
+            this.btn_Open.Text = "Abrir";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frm_PDFtoDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frm_PDFtoDB";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frm_PDFtoDB_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PDFtoDB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.TextBox textBox_Archivo;
+        private System.Windows.Forms.TextBox textBox_Nombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv_PDFtoDB;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ PDFtoDB/frmPDFtoDB.cs
@@ -0,0 +1,111 @@
+﻿using GestionDB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFtoDB
+{
+    public partial class frm_PDFtoDB : Form
+    {
+        GestionDB.XWingsFactoryEntities db;
+        public frm_PDFtoDB()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Todos los archivos (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox_Archivo.Text = openFileDialog1.FileName;
+            }
+            var a = db.AssemblyInstructions
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (textBox_Nombre.Text.Trim().Equals("") || textBox_Archivo.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("El nombre es obligatorio");
+                return;
+            }
+
+            byte[] file = null;
+            Stream myStream = openFileDialog1.OpenFile();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                myStream.CopyTo(ms);
+                file = ms.ToArray();
+            }
+
+            GestionDB.AssemblyInstructions oDocument = new GestionDB.AssemblyInstructions
+            {
+                idreference = textBox_Nombre.Text.Trim(),
+                Instructions = file,
+                id = openFileDialog1.SafeFileName,
+            };
+
+            db.AssemblyInstructions.Add(oDocument);
+            db.SaveChanges();
+            
+
+            Refresh();
+        }
+        private void Refresh()
+        {
+
+            //using (XWingsFactoryEntities db = new XWingsFactoryEntities())
+            //{
+            //    var lst = db.AssemblyInstructions.Select(x => x).ToList();
+            //    dgv_PDFtoDB.DataSource = lst;
+            //}
+        }
+
+        private void frm_PDFtoDB_Load(object sender, EventArgs e)
+        {
+            db = new GestionDB.XWingsFactoryEntities();
+            Refresh();
+            dgv_PDFtoDB.Columns[3].Visible = false;
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            if (dgv_PDFtoDB.Rows.Count > 0)
+            {
+                int id = int.Parse(dgv_PDFtoDB.Rows[dgv_PDFtoDB.CurrentRow.Index].Cells[0].Value.ToString());
+
+                using (GestionDB.XWingsFactoryEntities db = new GestionDB.XWingsFactoryEntities())
+                {
+                    var oDocument = db.AssemblyInstructions.Find(id);
+
+                    string path = AppDomain.CurrentDomain.BaseDirectory;
+                    string folder = path + "temp\\";
+                    string fullFilePath = folder + oDocument.realName;
+
+                    if (!Directory.Exists(folder))
+                        Directory.CreateDirectory(folder);
+
+                    if (File.Exists(fullFilePath))
+                        File.Delete(fullFilePath);
+
+                    File.WriteAllBytes(fullFilePath, oDocument.archivo);
+
+                    Process.Start(fullFilePath);
+                }
+            }
+        }
+    }
+}
