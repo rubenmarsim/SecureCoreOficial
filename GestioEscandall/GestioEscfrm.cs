e54bc0ecc30f024aff86d571582c8e02ba1c3e81
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-04-09 16:29:22 +0200

    join mas select de no usados

--- GestioEscandall/GestioEscfrm.cs
+++ GestioEscandall/GestioEscfrm.cs
@@ -15,126 +15,34 @@ namespace GestioEscandall
 {
     public partial class GestioEscfrm : Form
     {
+        #region Variables Globales
         bool primerCop = true;
         private string _Conec;
-        // SqlConnection con;
 
         XWingsFactoryEntities db;
+        #endregion Variables Globales
 
+        #region Constructores
+        /// <summary>
+        /// Constructor por defecto de la clase
+        /// </summary>
         public GestioEscfrm()
         {
             InitializeComponent();
         }
+        #endregion Constructores
 
-        ///////////////////// FORM LOAD /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #region Events
+        
+        #region Form load        
         private void GestioEscfrm_Load(object sender, EventArgs e)
         {
             db = new XWingsFactoryEntities();
             cargarComboPart();
         }
+        #endregion Form load
 
-        /////////////////////CONNEXIO BBDD/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //public void ConnectBD()
-        //{
-        //    _Conec = ConfigurationManager.ConnectionStrings["SecureCodeSplashScreen.Properties.Settings.XWingsFactoryConnectionString"].ConnectionString;
-        //    con = new SqlConnection(_Conec);
-        //    con.Open();
-        //}
-
-        //public DataTable PortaTaula(string selectQuery)
-        //{
-        //    ConnectBD();
-        //    SqlCommand cmd = new SqlCommand(selectQuery, con);
-        //    SqlDataAdapter mysqldt = new SqlDataAdapter(cmd);
-        //    DataTable dt = new DataTable();
-        //    mysqldt.Fill(dt);
-        //    return dt;
-        //}
-
-        //////////////////// FUNCIONS //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        public void cargarComboPart()
-        {
-            //var sQuery = db.ReferenceTypes.ToList();
-            //var aa = sQuery.Where(x => x.idReferenceType == 1 || x.idReferenceType == 2).Select(x => x.descReferenceType).ToList();
-            //string selectQuery = "SELECT * FROM [ReferenceTypes] where idReferenceType in (1,2);";
-            //DataTable dt = PortaTaula(selectQuery);
-            var dt = from r in db.ReferenceTypes
-                     where r.idReferenceType == 1 || r.idReferenceType == 2
-                     select r;
-
-            comboPart.DataSource = dt.ToList();
-            comboPart.ValueMember = "idReferenceType";
-            comboPart.DisplayMember = "descReferenceType";
-        }
-
-        public void cargarComboObject(string valor)
-        {
-            int num = Convert.ToInt32(valor);
-            //string selectQuery = "SELECT * FROM [REFERENCES] r, [ReferenceTypes] rt where rt.idReferenceType = r.idReferenceType and  r.idReferenceType = " + num;
-            //DataTable dt = PortaTaula(selectQuery);
-            var dt = from r in db.References// outer sequence
-                     join rt in db.ReferenceTypes //inner sequence 
-                     on r.idReferenceType equals rt.idReferenceType // key selector 
-                     where r.idReferenceType == num
-                     select r;
-
-            comboObject.DataSource = dt.ToList();
-            comboObject.ValueMember = "idReference";
-            comboObject.DisplayMember = "descReference";
-        }
-
-        public void cargarListUsats(string valor)
-        {
-            int num = Convert.ToInt32(valor);
-            //string selectQuery = "select descReference from [References] r, [Structure] s where idReferenceType = " + num + "and s.idReferenceFinal = r.idReference";
-            //DataTable dt = PortaTaula(selectQuery);
-
-            List<References> query = (from r in db.References// outer sequence
-                     join s in db.Structure //inner sequence 
-                     on r.idReference equals s.idReferenceFinal // key selector 
-                     where r.idReferenceType == num
-                     select r).ToList();
-
-            DataTable dt = new DataTable();
-            foreach (var item in query.ToList())
-            {
-                dt.Rows.Add(item.ToString());
-            }
-
-            //listNoUsat.DataSource = dt.ToList();
-
-            for (int i = 0; i < dt.Rows.Count; i++)
-            {
-                listUsats.Items.Add(dt.Rows[i][0].ToString());
-            }
-        }
-
-        public void cargarListNoUsat(string valor)
-        {
-            int num = Convert.ToInt32(valor);
-            //string selectQuery = "select r.descReference from [References] r, [Structure] s where r.idReference = s.idReferencePart and s.idReferenceFinal = " + num;
-            //DataTable dt = PortaTaula(selectQuery);
-
-            List<References> query = (from r in db.References// outer sequence
-                     join s in db.Structure //inner sequence 
-                     on r.idReference equals s.idReferencePart // key selector 
-                     where s.idReferenceFinal == num
-                     select r).ToList();
-
-            DataTable dt = new DataTable();
-
-            foreach (var item in query)
-            {
-                dt.Rows.Add(item.ToString());
-            }
-            //listUsats.DataSource = dt.ToList();
-
-            for (int i = 0; i < dt.Rows.Count; i++)
-            {
-                listNoUsat.Items.Add(dt.Rows[i][0].ToString());
-            }
-        }
-
+        #region DRAG & DROP
         //////////////////// DRAG & DROP //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         private void listUsats_MouseDown(object sender, MouseEventArgs e)
         {
@@ -205,8 +113,8 @@ namespace GestioEscandall
                 listUsats.Items.Add(str);
             }
         }
+        #endregion DRAG & DROP
 
-        /////////// EVENTS //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         private void derecha_Click(object sender, EventArgs e)
         {
 
@@ -253,7 +161,7 @@ namespace GestioEscandall
 
                 if (valor != null)
                 {
-                    cargarListUsats(valor);
+                    //cargarListUsats(valor);
                     cargarListNoUsat(valor);
                 }
             }
@@ -268,5 +176,95 @@ namespace GestioEscandall
             listUsats.Items.Clear();
             listNoUsat.Items.Clear();
         }
+        #endregion Events
+
+        #region Funcions
+        /// <summary>
+        /// Carga el comboBox de las partes principales e intermedias
+        /// </summary>
+        public void cargarComboPart()
+        {            
+            var FilteredtReferenceTypes = from r in db.ReferenceTypes
+                     where r.idReferenceType == 1 || r.idReferenceType == 2
+                     select r;
+
+            comboPart.DataSource = FilteredtReferenceTypes.ToList();
+            comboPart.ValueMember = "idReferenceType";
+            comboPart.DisplayMember = "descReferenceType";
+        }
+
+        public void cargarComboObject(string valor)
+        {
+            int num = Convert.ToInt32(valor);
+            //string selectQuery = "SELECT * FROM [REFERENCES] r, [ReferenceTypes] rt where rt.idReferenceType = r.idReferenceType and  r.idReferenceType = " + num;
+            //DataTable dt = PortaTaula(selectQuery);
+            var dt = from r in db.References// outer sequence
+                     join rt in db.ReferenceTypes //inner sequence 
+                     on r.idReferenceType equals rt.idReferenceType // key selector 
+                     where r.idReferenceType == num
+                     select r;
+
+            comboObject.DataSource = dt.ToList();
+            comboObject.ValueMember = "idReference";
+            comboObject.DisplayMember = "descReference";
+        }
+
+        public void cargarListUsats(string valor)
+        {
+            int num = Convert.ToInt32(valor);
+            //string selectQuery = "select descReference from [References] r, [Structure] s where idReferenceType = " + num + "and s.idReferenceFinal = r.idReference";
+            //DataTable dt = PortaTaula(selectQuery);
+
+            List<References> query = (from r in db.References// outer sequence
+                                      join s in db.Structure //inner sequence 
+                                      on r.idReference equals s.idReferenceFinal // key selector 
+                                      where r.idReferenceType == num
+                                      select r).ToList();
+
+            DataTable dt = new DataTable();
+            foreach (var item in query.ToList())
+            {
+                dt.Rows.Add(item.ToString());
+            }
+
+            //listNoUsat.DataSource = dt.ToList();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                listUsats.Items.Add(dt.Rows[i][0].ToString());
+            }
+        }
+
+        public void cargarListNoUsat(string valor)
+        {
+            var num = short.Parse(valor);
+            //string selectQuery = "select r.descReference from [References] r, [Structure] s where r.idReference = s.idReferencePart and s.idReferenceFinal = " + num;
+            //DataTable dt = PortaTaula(selectQuery);
+
+            var query = (from r in db.References// outer sequence
+                                      join s in db.Structure //inner sequence 
+                                      on r.idReference equals s.idReferencePart // key selector 
+                                      where s.idReferenceFinal == num
+                                      select r).ToList();
+
+
+            var JoinRefStruct = db.References.Join(db.Structure, refe => refe.idReference, strct => strct.idReferencePart, (refe, strct) => new { Referencia = refe, Structura = strct });
+
+            var lSubElementos = (JoinRefStruct.Where(x => x.Structura.idReferenceFinal == num).Select(x=>x.Referencia.descReference)).ToList();
+
+            //DataTable dt = new DataTable();
+
+            //foreach (var item in query)
+            //{
+            //    dt.Rows.Add(item.ToString());
+            //}
+            ////listUsats.DataSource = dt.ToList();
+
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    listNoUsat.Items.Add(dt.Rows[i][0].ToString());
+            //}
+        }
+        #endregion Funcions
     }
 }
