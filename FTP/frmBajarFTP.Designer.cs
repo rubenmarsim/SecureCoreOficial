553b1a79cdb19341f3a3788442839456fe62e1fb
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-05-10 20:35:41 +0200

    download

--- FTP/frmBajarFTP.Designer.cs
+++ FTP/frmBajarFTP.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(269, 90);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(288, 225);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // frmBajarFTP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "frmBajarFTP";
             this.Text = "frmBajarFTP";
+            this.Load += new System.EventHandler(this.frmBajarFTP_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDownload;
     }
 }
--- FTP/frmBajarFTP.cs
+++ FTP/frmBajarFTP.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region Usings
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+#endregion Usings
 
 namespace FTP
 {
+    /// <summary>
+    /// Formulario para bajar archivos del ftp
+    /// </summary>
     public partial class frmBajarFTP : Form
     {
+        #region Variables Globales
+        FtpWebRequest _Request;
+        FtpWebResponse _Response;
+        Stream _ResponseStream;
+        StreamReader _Reader;
+        const string _cstrDomain = "ftp://172.17.6.0";
+        const string _cstrUserName = "wookiecode";
+        const string _cstrPassword = "12345aA";
+        string _FileName = string.Empty;
+        #endregion Variables Globales
+
+        #region Constructors
         public frmBajarFTP()
         {
             InitializeComponent();
         }
+        #endregion Constructors
+
+        #region Events
+
+        private void frmBajarFTP_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            DownloadDoc();
+
+            var a = _Reader.ReadToEnd();
+
+            CloseConnections();
+        }
+
+        #endregion Events
+
+        #region Methods
+        /// <summary>
+        /// Descargamos el documento .edi con el que vamos a operar
+        /// </summary>
+        private void DownloadDoc()
+        {
+            _Request = (FtpWebRequest)WebRequest.Create(_cstrDomain + "/comandes.edi");
+            _Request.Credentials = new NetworkCredential(_cstrUserName, _cstrPassword);
+            _Request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            _Response = (FtpWebResponse)_Request.GetResponse();
+
+            _ResponseStream = _Response.GetResponseStream();
+            _Reader = new StreamReader(_ResponseStream);
+        }
+
+        /// <summary>
+        /// Close all connects
+        /// </summary>
+        private void CloseConnections() { _Reader.Close(); _Response.Close(); }
+        #endregion Methods
     }
 }
