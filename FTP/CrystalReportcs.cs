1bac16e357522064d027f9d334292737bd97c4b0
Toonii <toni.arandasds@gmail.com>
2019-05-20 18:15:58 +0200

    Crystaliko

---
+++ FTP/CrystalReportcs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP
+{
+    public partial class CrystalReportcs : Form
+    {
+        public CrystalReportcs()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
