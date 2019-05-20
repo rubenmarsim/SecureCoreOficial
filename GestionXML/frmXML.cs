449d317f81cd57154eedfebc56ffa98b223f217e
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-05-20 17:42:42 +0200

    creacion basica XML.

--- GestionXML/frmXML.cs
+++ GestionXML/frmXML.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +19,9 @@ namespace GestionXML
     {
         #region Variables Globales        
         GestionDB.SecureCoreDataSet db;
-        List<object> lTables;
+        ConnectionClass.ClassBDD BDD;
+        DataSet dts;
+        string _sResourcesPath;
         #endregion Variables Globales
 
         #region Constructores
@@ -32,25 +36,44 @@ namespace GestionXML
 
         #region Events
         private void frmXML_Load(object sender, EventArgs e)
-        {            
+        {
+            BDD = new ConnectionClass.ClassBDD();
             db = new GestionDB.SecureCoreDataSet();
+            _sResourcesPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).Replace("\\", "/") + "/Resources/";
         }
         private void btnCreateXML_Click(object sender, EventArgs e)
         {
             GetTablas();
+            WriteXML();
         }
         #endregion Events
 
         #region Methods
         private void GetTablas()
         {
-            var a = db;
-            lTables = new List<object>();
-            lTables.Add(db.Routes.ToList());
-            lTables.Add(db.DefinedRoutes.ToList());
-            lTables.Add(db.Filiations.ToList());
-            lTables.Add(db.Regions.ToList());
-            lTables.Add(db.Planets.ToList());
+            SqlConnection con = new SqlConnection("Data Source=wookie-code.database.windows.net;Initial Catalog=SecureCore;User ID=Wookie;Password=123456789aA"); //"GestionDB.Properties.Settings.SecureCoreConnectionString"
+            dts = new DataSet();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Routes; select * from DefinedRoutes; select * from Filiations; select * from Regions; select * from Planets", con);
+            adapter.TableMappings.Add("Table", "Route");
+            adapter.TableMappings.Add("Table1", "DefinedRoute");
+            adapter.TableMappings.Add("Table2", "Filiations");
+            adapter.TableMappings.Add("Table3", "Regions");
+            adapter.TableMappings.Add("Table4", "Planets");
+            adapter.Fill(dts);
+        }
+
+        private void WriteXML()
+        {
+            try
+            {                
+                dts.WriteXml(_sResourcesPath + "Test.xml");
+                MessageBox.Show("XML generat correctament.");
+            }
+            catch (Exception Ge)
+            {
+                MessageBox.Show(Ge.Message);
+            }            
         }
         #endregion Methods
     }
